81181b04d94f6a6922bac1bf5eb62b7dc96f7ca5
Maksim <grigoriev.ma2014@yandex.ru>
2022-07-20 11:01:28 +0300

    added redux

--- src/App.js
+++ src/App.js
@@ -1,27 +1,51 @@
 import React from 'react';
 import './scss/app.scss';
 import Header from './components/Header'
-
 import { Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import NotFond from './pages/NotFond';
 import Cart from './pages/Cart';
 
+import { useSelector, useDispatch } from 'react-redux'
+
+import { decrement, increment, test } from './redux/slices/filterSlice.js'
 
 export const SearchContext = React.createContext('')
-
-
 
 
 function App() {
 
   const [searchValue, setSearchValue] = React.useState('')
 
+  const count = useSelector((state) => state.counter.count)
+  const dispatch = useDispatch()
+
+
+
+
 
 
 
   return (
+
+
+
     <div className="wrapper">
+
+      <button
+        aria-label="Increment value"
+        onClick={() => dispatch(test())}
+      >
+        Increment
+      </button>
+      <span>{count}</span>
+      <button
+        aria-label="Decrement value"
+        onClick={() => dispatch(decrement())}
+      >
+        Decrement
+      </button>
+
       <SearchContext.Provider value={{ searchValue, setSearchValue }}>
         <Header />
         <div className="content">
---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+import counterReducer from './slices/filterSlice.js'
+
+export const store = configureStore({
+
+  reducer: {
+    counter: counterReducer
+  },
+
+})
